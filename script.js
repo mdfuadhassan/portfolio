a379806543d41cf9b29b91a4aad7f8565b830db9
Fuad Hassan <141770454+mdfuadhassan@users.noreply.github.com>
2023-09-28 22:55:32 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+//  Setup and start animation!
+var typed = new Typed('#element', {
+    strings: ['Font-end Developer', 'Web Developer', 'Web Designer', 'Python Developer', 'Javascript Developer'],
+    typeSpeed: 50,
+});
+
+// Sidebar script
+function barsFunction() {
+    var pageLink = document.querySelector(".page-link");
+    pageLink.style.right = "5px";
+
+    var barsIcon = document.querySelector("#bars-icon");
+    barsIcon.style.display = "none";
+
+    var barsIcon2 = document.querySelector("#bars-icon2");
+    barsIcon2.style.display = "block";
+};
+
+
+function barsFunction2() {
+    var pageLink = document.querySelector(".page-link");
+    pageLink.style.right = "-170px";
+
+    var barsIcon = document.querySelector("#bars-icon");
+    barsIcon.style.display = "block";
+
+    var barsIcon2 = document.querySelector("#bars-icon2");
+    barsIcon2.style.display = "none";
+};
